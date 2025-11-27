143c252f26e0f3cacd09371bec31b762157e8dd4
Liam <liam5061@hotmail.com>
2025-11-27 21:01:27 +0000

    Initial project commit

---
+++ src/main/java/com/worldreports/world_reports/WorldReportsApplication.java
@@ -0,0 +1,13 @@
+package com.worldreports.world_reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worldreports/world_reports/WorldReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldreports.world_reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
